346d1eca53fff30f9a9cf0ad366274d2b16c1622
Ilya <rogachevskii01@gmail.com>
2022-08-19 20:27:58 +0600

    Я устал

--- React/marvel_api/src/components/charList/CharList.js
+++ React/marvel_api/src/components/charList/CharList.js
@@ -37,10 +37,10 @@ class CharList extends Component {
     }
 
     render() {
-        const {characters, loading, error} = this.state;
+        const {loading, error} = this.state;
         const errorMessage = error ? <ErrorMessage/> : null;
         const spinner = loading ? <Spinner/> : null;
-        const content = !(loading || error) ? <View characters={characters}/> : null;
+        const content = !(loading || error) ? this.view() : null;
 
         return (
             <div className="char__list">
@@ -53,28 +53,31 @@ class CharList extends Component {
             </div>
         )
     }
-}
 
-const View = ({characters}) => {
-    const elements = characters.map(item => {
-        const {thumbnail, name} = item;
-        let style = 'cover';
-        if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
-            style = 'contain';
-        }
+    view = () => {
+        const elements = this.state.characters.map(item => {
+            const {thumbnail, name, id} = item;
+            let style = 'cover';
+            if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+                style = 'unset';
+            }
+            return (
+                <li 
+                    className="char__item"
+                    key={id}
+                    onClick={() => this.props.onCharSelected(id)}>
+                        <img src={thumbnail} style={{'objectFit' :style}} alt={name}/>
+                        <div className="char__name">{name}</div>
+                </li>
+            )
+        })
+    
         return (
-            <li key={name} className="char__item">
-                <img src={thumbnail} style={{'objectFit' :style}} alt={name}/>
-                <div className="char__name">{name}</div>
-            </li>
+            <ul className="char__grid">
+                {elements}
+            </ul>
         )
-    })
-
-    return (
-        <ul className="char__grid">
-            {elements}
-        </ul>
-    )
+    }
 }
 
 export default CharList;
